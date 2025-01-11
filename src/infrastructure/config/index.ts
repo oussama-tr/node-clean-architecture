d10ea0f251b44dc58c25fbdd125605eb28d920b0
Oussama Trabelsi <oussama.trabelsidev@gmail.com>
2025-01-11 18:16:08 +0100

    chore: change prettier tab width

--- src/infrastructure/config/index.ts
+++ src/infrastructure/config/index.ts
@@ -4,17 +4,8 @@ import { z } from 'zod';
 export function makeConfig(): ApplicationConfig {
   const schema = z.object({
     DATABASE_URL: z.string(),
-    NODE_ENV: z.union([
-      z.literal('development'),
-      z.literal('production'),
-      z.literal('test'),
-    ]),
-    LOG_LEVEL: z.union([
-      z.literal('debug'),
-      z.literal('info'),
-      z.literal('warn'),
-      z.literal('error'),
-    ]),
+    NODE_ENV: z.union([z.literal('development'), z.literal('production'), z.literal('test')]),
+    LOG_LEVEL: z.union([z.literal('debug'), z.literal('info'), z.literal('warn'), z.literal('error')]),
     PORT: z
       .string()
       .transform((val) => parseInt(val, 10))
