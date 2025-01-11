b22d44eaeb8b38cced287184b971065c9991c937
Oussama Trabelsi <oussama.trabelsidev@gmail.com>
2025-01-11 17:12:02 +0100

    feat: get post buisness logic

--- src/application/posts/index.ts
+++ src/application/posts/index.ts
@@ -1,11 +1,15 @@
 import { makeCreatePostCommand } from './commands/create-post';
 import { makeDeletePostCommand } from './commands/delete-post';
+import { makeGetPostQuery } from './queries/get-post';
 
 export function makePostsUseCases(dependencies: Dependencies) {
   return {
     commands: {
       createPost: makeCreatePostCommand(dependencies),
       deletePost: makeDeletePostCommand(dependencies),
+    },
+    queries: {
+      getPost: makeGetPostQuery(dependencies),
     },
   };
 }
