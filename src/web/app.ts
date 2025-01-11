b6eaf95d4e9a5a90574791bc824cf2a1260c37cc
Oussama Trabelsi <oussama.trabelsidev@gmail.com>
2025-01-11 18:24:28 +0100

    feat: swagger and error handler plugins

--- src/web/app.ts
+++ src/web/app.ts
@@ -1,8 +1,10 @@
+import AutoLoad from '@fastify/autoload';
 import { diContainer, fastifyAwilixPlugin } from '@fastify/awilix';
 import Cors from '@fastify/cors';
 import Helmet from '@fastify/helmet';
 import { makeInfrastructureDependencies } from '@infrastructure/di';
 import { FastifyInstance } from 'fastify';
+import { join } from 'path';
 
 export default async function makeApp(fastify: FastifyInstance) {
   // Create a dependency injection container
@@ -20,6 +22,18 @@ export default async function makeApp(fastify: FastifyInstance) {
   // Configure CORS
   await fastify.register(Cors, {
     origin: false, // TODO: Set this to a valid origin
+  });
+
+  // Auto-load plugins
+  await fastify.register(AutoLoad, {
+    dir: join(__dirname, 'plugins'),
+    dirNameRoutePrefix: false,
+  });
+
+  // Auto-load routes
+  await fastify.register(AutoLoad, {
+    dir: join(__dirname, 'routes'),
+    dirNameRoutePrefix: false,
   });
 
   return fastify;
