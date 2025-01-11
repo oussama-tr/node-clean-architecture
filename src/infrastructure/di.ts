49e1937d53609ae05213a52763826c3ff6122417
Oussama Trabelsi <oussama.trabelsidev@gmail.com>
2025-01-11 16:41:51 +0100

    feat: posts repository

--- src/infrastructure/di.ts
+++ src/infrastructure/di.ts
@@ -1,14 +1,16 @@
 import * as Interfaces from '@application/common/interfaces';
 import { makeConfig } from '@infrastructure/config';
 import { PrismaClient } from '@prisma/client';
-import { Resolver, asValue } from 'awilix';
+import { Resolver, asFunction, asValue } from 'awilix';
 
 import { makeLogger } from './logger';
+import * as repositories from './repositories';
 
 export type Dependencies = {
   config: Interfaces.ApplicationConfig;
   db: PrismaClient;
   logger: Interfaces.Logger;
+  postsRepository: Interfaces.PostsRepository;
 };
 
 export function makeInfrastructureDependencies(): {
@@ -29,5 +31,6 @@ export function makeInfrastructureDependencies(): {
     config: asValue(config),
     db: asValue(db),
     logger: asValue(logger),
+    postsRepository: asFunction(repositories.makePostsRepository).singleton(),
   };
 }
